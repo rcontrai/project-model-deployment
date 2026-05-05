7c6b587804bf8e03f271ceae4d7dbedc7071f416
rcontrai <romain.contrain@free.fr>
2026-05-05 18:28:57 +0200

    fix ref to Dockerfile

--- build-docker.sh
+++ build-docker.sh
@@ -1,2 +1,2 @@
 . ./envars.sh
-docker build -f dockerfile -t dockerized_model_api --build-arg API_PORT="$API_PORT" .
+docker build -f Dockerfile -t dockerized_model_api --build-arg API_PORT="$API_PORT" .
