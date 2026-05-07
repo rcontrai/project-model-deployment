06991810c68b3e8de5db93a638f93cabe24ffa0a
rcontrai <romain.contrain@free.fr>
2026-05-07 18:03:11 +0200

    update local test build scripts

--- build-docker.sh
+++ build-docker.sh
@@ -1,2 +1,6 @@
 . ./envars.sh
-docker build -f Dockerfile -t dockerized_model_api --build-arg APP_PORT="$APP_PORT" .
+# # Image qui expose l'API (il faut modifier le dockerfile)
+# docker build -f Dockerfile -t dockerized_model_api --build-arg APP_PORT="8000" .
+
+# Image qui contient l'UI
+docker build -f Dockerfile -t dockerized_model_ui --build-arg APP_PORT="$APP_PORT" .
--- run-docker.sh
+++ run-docker.sh
@@ -1,2 +1,5 @@
 . ./envars.sh
-docker run -p "$APP_PORT":"$APP_PORT" dockerized_model_api
+# # Image qui expose l'API pour tester l'UI plus vite
+docker run -p "8000":"8000" dockerized_model_api
+# # Image qui contient l'UI
+# docker run -p "$APP_PORT":"$APP_PORT" dockerized_model_ui
